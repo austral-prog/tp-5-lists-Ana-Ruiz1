40a40f470fbef656fdcf4a7e9ead1414fa282189
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-04-18 18:21:19 -0300

    Update exercise_remove_elements.py

--- exercise_remove_elements.py
+++ exercise_remove_elements.py
@@ -1,16 +1,10 @@
 # Ejercicio 4: Remover elementos en posiciones específicas
 
 def remove_elements(lista):
-    """
-    Remueve el primer, quinto y sexto elemento de la lista.
-    La función debe funcionar con listas de cualquier tamaño.
-
-    Args:
-        lista: Una lista de elementos
-
-    Returns:
-        La lista después de remover los elementos indicados
-    """
-    lista[4:6] = []
-    lista[0] = []
+    if len(lista) > 5:
+        lista.pop(5) 
+    if len(lista) > 4:
+        lista.pop(4) 
+    if len(lista) > 0:
+        lista.pop(0) 
     return lista
